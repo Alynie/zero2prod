5d24a44920eb2bdd8c552c77f847eb405721b6d4
Alynie <alynie.walter@gmail.com>
2025-05-31 17:17:49 -0500

    add first integration test

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-use dotenv::dotenv;
-use std::env;
-fn main() {
-    dotenv().ok();
-    let test_var = env::var("ENV_TEST");
-
-    match test_var {
-        Ok(val) => println!("ENV_TEST: {:?}", val),
-        Err(e) => println!("Error ENV_TEST: {}", e),
-    }
-    println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use zero2prod::run;
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    run(None)?.await
 }
